93f66fce453e6792eee7beefcdfaf44b4dbc6b8f
elixter <ltw971@naver.com>
2020-12-21 16:21:57 +0900

    백준 2581번 계단오르기

---
+++ Baekjoon/2579.cpp
@@ -0,0 +1,42 @@
+//// 백준 알고리즘 2579번 계단 오르기
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//#include <iostream>
+//#include <climits>
+//#include <cmath>
+//#include <unordered_set> 
+//
+//using namespace std;
+//
+//int max(const int a, const int b) {
+//	if (a > b) return a;
+//	else return b;
+//}
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//
+//	int N;			// Number of players
+//	cin >> N;
+//	int stairs[301] = { 0 };
+//	int dp[301] = { 0 };
+//
+//
+//	for (int i = 1; i <= N; i++) {
+//		cin >> stairs[i];
+//	}
+//
+//	dp[1] = stairs[1];
+//	dp[2] = stairs[1] + stairs[2];
+//
+//	for (int i = 3; i <= N; i++) {
+//		dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
+//	}
+//
+//	cout << dp[N] << '\n';
+//
+//	return 0;
+//}
--- Baekjoon/Main.cpp
+++ Baekjoon/Main.cpp
@@ -6,54 +6,36 @@
 #include <iostream>
 #include <climits>
 #include <cmath>
+#include <unordered_set> 
 
 using namespace std;
+
+int max(const int a, const int b) {
+	if (a > b) return a;
+	else return b;
+}
 
 int main() {
 	ios::sync_with_stdio(0);
 
 	int N;			// Number of players
 	cin >> N;
-
-	bool select[20] = { 0 };
-	int stats[20][20] = { 0 };
-
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j < N; j++) {
-			cin >> stats[i][j];
-		}
+	int stairs[301] = { 0 };
+	int dp[301] = { 0 };
+	
+	
+	for (int i = 1; i <= N; i++) {
+		cin >> stairs[i];
 	}
 
-	// 조합 생성을 위한 배열 초기화 => nCn/2
-	for (int i = 0; i < N / 2; i++) {
-		select[i] = 1;
+	dp[1] = stairs[1];
+	dp[2] = stairs[1] + stairs[2];
+
+	for (int i = 3; i <= N; i++) {
+		dp[i] = max(dp[i - 2] + stairs[i], dp[i - 3] + stairs[i - 1] + stairs[i]);
 	}
 
-	// 선수 등번호
-	int number[20];
-	int min = INT_MAX;
-	do {
-		for (int i = 0, s = 0, l = N / 2; i < N; i++) {
-			if (select[i]) number[s++] = i;
-			else number[l++] = i;
-		}
-		
-		int start = 0; int link = 0;
-		for (int i = 0; i < N / 2 - 1; i++) {
-			for (int j = i + 1; j < N / 2; j++) {
-				start += stats[number[i]][number[j]] + stats[number[j]][number[i]];
-				link += stats[number[i + N / 2]][number[j + N / 2]] + stats[number[j + N / 2]][number[i + N / 2]];
-			}
-		}
-		int diff = abs(start - link);
-		if (diff == 0) {
-			cout << 0 << '\n';
-			return 0;
-		}
-		if (diff < min) min = diff;
-	} while (prev_permutation(select, select + N));
-
-	cout << min << '\n';
+	cout << dp[N] << '\n';
 
 	return 0;
 }
