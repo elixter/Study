3fd314eafc2189304a6032a65dff399d61f7a31d
elixter <ltw971@naver.com>
2020-12-24 17:07:22 +0900

    백준 10844 쉬운 계단 수

---
+++ Baekjoon/10844.cpp
@@ -0,0 +1,50 @@
+//#define _CRT_SECURE_NO_WARNINGS
+//
+//#include <string>
+//#include <vector>
+//#include <algorithm>
+//#include <iostream>
+//#include <climits>
+//#include <cmath>
+//#include <unordered_set> 
+//
+//using namespace std;
+//
+//int max(const int a, const int b) {
+//	if (a > b) return a;
+//	else return b;
+//}
+//
+//long long dp[101][10] = { 0 };
+//
+//#define MOD 1000000000
+//
+//int main() {
+//	ios::sync_with_stdio(0);
+//
+//	int N;
+//	cin >> N;
+//
+//	for (int i = 1; i < 10; i++) {
+//		dp[1][i] = 1;
+//	}
+//	dp[2][0] = 1, dp[2][1] = 1, dp[2][2] = 2, dp[2][3] = 2, dp[2][4] = 2, dp[2][4] = 2,
+//		dp[2][5] = 2, dp[2][6] = 2, dp[2][7] = 2, dp[2][8] = 2, dp[2][9] = 1;
+//
+//	for (int i = 3; i <= N; i++) {
+//		dp[i][0] = dp[i - 1][1] % MOD;
+//		dp[i][9] = dp[i - 1][8] % MOD;
+//		for (int j = 1; j <= 8; j++) {
+//			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
+//		}
+//	}
+//
+//	long long result = 0;
+//	for (int i = 0; i < 10; i++) {
+//		result += dp[N][i];
+//	}
+//
+//	cout << result % MOD << '\n';
+//
+//	return 0;
+//}
--- Baekjoon/Main.cpp
+++ Baekjoon/Main.cpp
@@ -15,32 +15,36 @@ int max(const int a, const int b) {
 	else return b;
 }
 
-int tri[500][500] = { 0 };
-int dp[500][500] = { 0 };
+long long dp[101][10] = { 0 };
+
+#define MOD 1000000000
 
 int main() {
 	ios::sync_with_stdio(0);
 
-	int N;
+	int N; 
 	cin >> N;
 
+	for (int i = 1; i < 10; i++) {
+		dp[1][i] = 1;
+	}
+	dp[2][0] = 1, dp[2][1] = 1, dp[2][2] = 2, dp[2][3] = 2, dp[2][4] = 2, dp[2][4] = 2,
+		dp[2][5] = 2, dp[2][6] = 2, dp[2][7] = 2, dp[2][8] = 2, dp[2][9] = 1;
 
-	for (int i = 0; i < N; i++) {
-		for (int j = 0; j <= i; j++) {
-			cin >> tri[i][j];
-			if (i == N - 1) {
-				dp[i][j] = tri[i][j];
-			}
+	for (int i = 3; i <= N; i++) {
+		dp[i][0] = dp[i - 1][1] % MOD;
+		dp[i][9] = dp[i - 1][8] % MOD;
+		for (int j = 1; j <= 8; j++) {
+			dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j + 1]) % MOD;
 		}
 	}
-
-	for (int i = N - 2; i >= 0; i--) {
-		for (int j = i; j >= 0; j--) {
-			dp[i][j] = tri[i][j] + max(dp[i + 1][j], dp[i + 1][j + 1]);
-		}
+	
+	long long result = 0;
+	for (int i = 0; i < 10; i++) {
+		result += dp[N][i];
 	}
 
-	cout << dp[0][0] << '\n';
+	cout << result % MOD << '\n';
 
 	return 0;
 }
